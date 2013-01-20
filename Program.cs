ffb0793b99a317fc494952715de81f0818ce794f
Tripp Hill <trippenator0@gmail.com>
2013-01-20 02:09:55 -0500

    third

--- Program.cs
+++ Program.cs
@@ -143,15 +143,9 @@ namespace HelloWorld
             try
             {
 
-                // Create an unconnected socket since if invoked as a receiver we don't necessarily
+               
 
-                //    want to associate the socket with a single endpoint. Also, for a sender socket
-
-                //    specify local port of zero (to get a random local port) since we aren't receiving
-
-                //    anything.
-
-                Console.WriteLine("Creating connectionless socket...");
+                Console.WriteLine("creating null socket...");
 
                 if (udpSender == false)
 
@@ -163,7 +157,6 @@ namespace HelloWorld
 
 
 
-                // Join any multicast groups specified
                 byte[] byData = System.Text.Encoding.ASCII.GetBytes(sendString);
    				 
 
@@ -175,13 +168,7 @@ namespace HelloWorld
                     if (localAddress.AddressFamily == AddressFamily.InterNetwork)
                     {
 
-                        // For IPv4 multicasting only the group is specified and not the
-
-                        //    local interface to join it on. This is bad as on a multihomed
-
-                        //    machine, the application won't really know which interface
-
-                        //    it is joined on (and there is no way to change it via the UdpClient).
+                        
 
                         udpSocket.JoinMulticastGroup((IPAddress)multicastGroups[i]);
 
@@ -190,15 +177,6 @@ namespace HelloWorld
                     else if (localAddress.AddressFamily == AddressFamily.InterNetworkV6)
                     {
 
-                        // For some reason, the IPv6 multicast join allows the local interface index
-
-                        //    to be specified such that the application can join multiple groups on
-
-                        //    any interface which is great (but lacking in IPv4 multicasting with the
-
-                        //    UdpClient). IPv6 multicast groups should be specified with the scope id
-
-                        //    when passed on the command line (e.g. fe80::1%4).
 
                         udpSocket.JoinMulticastGroup((int)((IPAddress)multicastGroups[i]).ScopeId, (IPAddress)multicastGroups[i]);
 
@@ -206,15 +184,6 @@ namespace HelloWorld
 
                 }
 
-
-
-                // If you want to send data with the UdpClient it must be connected -- either by
-
-                //    specifying the destination in the UdpClient constructor or by calling the
-
-                //    Connect method. You can call the Connect method multiple times to associate
-
-                //    a different endpoint with the socket.
 
                 if (udpSender == true)
                 {
@@ -229,60 +198,15 @@ namespace HelloWorld
 
                 if (udpSender == true)
                 {
-
-                    // Send the requested number of packets to the destination
-
-                   /* Console.WriteLine("Sending the requested number of packets to the destination, Send()...");
-                    byte[] sendBy = Encoding.ASCII.GetBytes(sendString);
-                    foreach(byte element in sendBy) {
-                        Console.WriteLine("{0} = {1}", element, (char)element);
-                    }
-                    for (i = 0; i < sendCount; i++)
-                    {
-                        
-                        rc = udpSocket.Send(sendBy, sendBy.Length);
-
-                        Console.WriteLine("Sent {0} bytes to {1}", rc, destAddress.ToString());
-
-                    }
-*
-                    // Send a few zero length datagrams to indicate to the receive to exit. Put a short
-
-                    //    sleep between them since UDP is unreliable and zero byte datagrams are really
-
-                    //    fast (and the local stack can actually drop datagrams before they even make it
-
-                    //    onto the wire).
-
-                    Console.WriteLine("HAving some sleep, Sleep(250)...");
-                
-
-                    /*for (i = 0; i < 3; i++)
-                    {
-
-                        rc = udpSocket.Send(sendBuffer, 0);
-
-                        System.Threading.Thread.Sleep(250);
-
-                    }
-
-                }*/
+                    //send
 					udpSocket.Send(byData,byData.Length);
 				}
                 else
                 {
-
+					//recieve
                     IPEndPoint senderEndPoint = new IPEndPoint(localAddress, 0);
 
-                    // Receive datagrams in a loop until a zero byte datagram is received.
-
-                    //    Note the difference with the UdpClient in that the source address
-
-                    //    is simply an IPEndPoint that doesn't have to be cast to and EndPoint
-
-                    //    object as is the case with the Socket class.
-
-                   // Console.WriteLine("Receiving datagrams in a loop until a zero byte datagram is received...");
+           
 					string machineName;
                     while (true)
                     {
@@ -345,9 +269,7 @@ namespace HelloWorld
                 if (udpSocket != null)
                 {
 
-                    // Clean things up by dropping any multicast groups we joined
-
-                   // Console.WriteLine("Cleaning things up by dropping any multicast groups we joined...");
+                  
 					Console.WriteLine("Successful, cleaning up..");
 
                     for (i = 0; i < multicastGroups.Count; i++)
@@ -363,9 +285,6 @@ namespace HelloWorld
                         else if (localAddress.AddressFamily == AddressFamily.InterNetworkV6)
                         {
 
-                            // IPv6 multicast groups should be specified with the scope id when passed
-
-                            //    on the command line
 
                             udpSocket.DropMulticastGroup(
 
@@ -379,9 +298,8 @@ namespace HelloWorld
 
                     }
 
-                    // Free up the underlying network resources
 
-                    Console.WriteLine("Sucess: Closing the socket.");
+                    Console.WriteLine("Success: closing the socket.");
 
                     udpSocket.Close();
 
